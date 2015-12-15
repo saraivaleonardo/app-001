748978d3d10a2d06a58b529f64b2e8ba7ab6e3c2
Leonardo Saraiva <leonardo.saraiva@tre-pi.jus.br>
2015-12-15 14:53:32 -0300

    app-011

---
+++ app-001/src/main/java/br/jus/trepi/app/PessoaBean.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.jus.trepi.app;
+
+import javax.annotation.ManagedBean;
+/**
+ *
+ * @author leonardo.saraiva
+ */
+@ManagedBean
+public class PessoaBean {
+    
+}
